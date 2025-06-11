5710dea79f7bf4a7d13e54ab586eaba429e46649
Exoucia M <63007560+exouciam@users.noreply.github.com>
2025-06-12 00:27:46 +0200

    microservice loans done, addede rest apis, entities, dto,exceptions, mapper, repo, contants, service implementation

--- loans/src/main/java/com/easybank/loans/LoansApplication.java
+++ loans/src/main/java/com/easybank/loans/LoansApplication.java
@@ -1,13 +1,31 @@
 package com.easybank.loans;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+/*@ComponentScans({ @ComponentScan("com.easybank.loans.controller") })
+@EnableJpaRepositories("com.easybank.loans.repository")
+@EntityScan("com.easybank.loans.model")*/
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Loans microservice REST API Documentation",
+                description = "Easy Bank Loans microservice REST API Documentation",
+                version = "v1",
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "https://www.easybank.com"
+                )
+        )
+)
 public class LoansApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LoansApplication.class, args);
     }
-
 }
